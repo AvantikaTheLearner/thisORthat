47c82e5ed94f5813054506287d025c4c50580671
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-12-02 12:53:40 -0500

    Added more seeds for questions and options tables

--- backend/db/seeds/03_questions.sql
+++ backend/db/seeds/03_questions.sql
@@ -5,4 +5,9 @@ VALUES
 (2, 5, 'Which food has more proteins?'),
 (3, 6, 'Who is current premier of Ontario?'),
 (4, 7, 'What is the sum of 5 and 2?'),
-(5, 8, 'How many covid cases been reported today?');
+(5, 8, 'How many covid cases been reported today?'),
+(1, 6, 'What is full form of LHL?'),
+(2, 1, 'Which web series should I watch?'),
+(3, 7, 'Which one should I choose to pursue my career'),
+(4, 3, 'Which book is must to read'),
+(5, 4, 'Which brand is best for having subtle make-up?');
--- backend/db/seeds/04_options.sql
+++ backend/db/seeds/04_options.sql
@@ -10,4 +10,14 @@ VALUES
 (4, '7'),
 (4, '5'),
 (5, '1200'),
-(5, '1500');
+(5, '1500'),
+(6, 'Light House Labs'),
+(6, 'Live Happy Life'),
+(7, 'Money Heist'),
+(7, 'Squid Game'),
+(8, 'Machine Learning'),
+(8, 'Data Science'),
+(9, 'Stay Hungry Stay Foolish'),
+(9, 'Think Rich'),
+(10, 'Huda'),
+(10, 'Mac');
